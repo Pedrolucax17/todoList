1042c9e6c8c5413f83e455e68d5afbb207520229
Pedro Lucas <pl.lucassouza@gmail.com>
2025-03-17 17:34:11 -0300

    Refatorando a classe Task com anotacoes

--- todolist/src/main/java/com/automind/todolist/model/Task.java
+++ todolist/src/main/java/com/automind/todolist/model/Task.java
@@ -1,12 +1,21 @@
 package com.automind.todolist.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Data
+@Entity
 public class Task {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
@@ -15,8 +24,10 @@ public class Task {
 
     private Boolean completed;
 
+    @CreationTimestamp
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
 }
